de0a01e9347a927303bcb593596fcfe521a9a37f
cezara98t <52743557+cezara98t@users.noreply.github.com>
2020-03-28 14:50:35 +0200

    Added neohouse panel

--- Assets/Scripts/Model/ActionButtonData.cs
+++ Assets/Scripts/Model/ActionButtonData.cs
@@ -2,19 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActionButtonData
+public class ActionButtonData : ButtonData
 {
-    public string text { get; set; }
-    public string subtext { get; set; }
     public string requested_ability { get; set; }
     public double requested_ability_percentage { get; set; }
-    public int affected_food { get; set; } //e cu + daca actiunea aduce, e cu - daca actiunea are nevoie
-    public int affected_resources { get; set; }
-    public int affected_people { get; set; }
     public string bad_message { get; set; }
     public int lose_food { get; set; }
     public int lose_resources { get; set; }
     public int lose_people { get; set; }
-    public Chromozome affected_chromozome { get; set; }
+    public Chromozome lose_affected_chromozome { get; set; }
     
 }
---
+++ Assets/Scripts/Model/ButtonData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ButtonData
+{
+    public string text { get; set; }
+    public string subtext { get; set; }
+    public int affected_food { get; set; } 
+    public int affected_resources { get; set; }
+    public int affected_people { get; set; }
+    public Chromozome affected_chromozome { get; set; }
+}
+
