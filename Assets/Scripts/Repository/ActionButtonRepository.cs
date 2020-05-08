d44e1e54cbd6180069e30606178b03dc7b4f4a12
cezara98t <52743557+cezara98t@users.noreply.github.com>
2020-05-08 21:54:48 +0300

    architecture

---
+++ Assets/Scripts/Repository/ActionButtonRepository.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public class ActionButtonRepository : IRepository<List<ActionButtonData>>
+{
+    public List<ActionButtonData> buttons { get; set; }
+
+    public void read()
+    {
+        string filename = GameData.mainPanel.name+"/"+GameData.currentJobPanel.name;
+        TextAsset t = Resources.Load<TextAsset>("Jsons/" + filename);
+        buttons = JsonConvert.DeserializeObject<List<ActionButtonData>>(t.ToString());
+    }
+}
+
---
+++ Assets/Scripts/Repository/ImprovementsRepository.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ImprovementsRepository : IRepository<List<ButtonData>>
+{
+    public List<ButtonData> buttons { get; set; }
+
+    public ImprovementsRepository()
+    {
+        read();
+    }
+    public void read()
+    {
+        TextAsset t = Resources.Load<UnityEngine.TextAsset>("Jsons/improvements");
+        buttons = JsonConvert.DeserializeObject<List<ButtonData>>(t.ToString());
+    }
+}
+
+
+
